917280ba415f0afa009d515c85a3e3fea6aaec10
winecory-code <winecory@nhsdlions.org>
2026-06-01 09:01:06 -0400

    Correct HTML doctype and jQuery selector
    Fix HTML syntax and jQuery selector for h1.

--- my_code_intro.js
+++ my_code_intro.js
@@ -1,10 +1,8 @@
-<DOCTYPE>
-
 $(document).ready(function() {
-  // hide the p tags
+
   $("p").hide();
 
-  $("h1).click(function() {
+  $("h1").click(function() {
   $(this).next().slideToggle(300) ;
   });
   });
