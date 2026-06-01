4c538da2d55b8cd713e1b825cfd5d5e2616f9f95
winecory-code <winecory@nhsdlions.org>
2026-06-01 08:25:36 -0400

    Update U7M1.js

--- U7M1.js
+++ U7M1.js
@@ -25,6 +25,19 @@
 <body>
 
 
+
+
+
+$(document).ready(function() {
+  // hide the p tags
+  $("p").hide();
+
+  $("h1).click(function() {
+  $(this).next().slideToggle(300) ;
+  });
+  });
+
+
   
 
 
