5666572414dbb6b8263ce428922b86ff409655d2
winecory-code <winecory@nhsdlions.org>
2026-06-10 10:42:27 -0400

    Create U7-project.js

---
+++ U7-project.js
@@ -0,0 +1,23 @@
+$(document) .ready(function() {
+  $("h1) .mouse click(function() {
+    $("this").css("background-color","Yellow") ;
+}) ; 
+  $("h3) .mouse click(function() {
+    $("this").css("background-color","yellow")
+    $(" ").unbind("mouseleave") ;
+}) ; 
+
+ $("h3) .mouse click(function() {
+    $("this").css("background-color","yellow")
+    $(" ").unbind("mouseleave") ;
+}) ; 
+
+ $("h3) .mouse click(function() {
+    $("this").css("background-color","yellow")
+    $(" ").unbind("mouseleave") ;
+}) ; 
+
+ $("h4) .mouse click(function() {
+    $("this").css("background-color","yellow")
+    $(" ").unbind("mouseleave") ;
+}) ; 
