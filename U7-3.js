2edcc890b3320494321b777ad7ebf01ab8974adf
winecory-code <winecory@nhsdlions.org>
2026-06-09 18:52:06 -0700

    Create U7-3.js

---
+++ U7-3.js
@@ -0,0 +1,7 @@
+$(document) .ready(function() {
+  $("h1) .mousedown(function() {
+    $("this").css("background-color","red") ;
+}) ; 
+  $("h1) .mousedown(function() {
+    $("this").css("background-color","yellow") ;
+}) ; 
